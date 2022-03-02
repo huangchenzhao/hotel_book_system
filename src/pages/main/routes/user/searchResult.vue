f321ae2b3661dcb5393c700c8b1092630ebc2fdc
mr <18292166117@163.com>
2022-03-02 11:21:14 +0800

    展示酒店搜索结果的图片和星级

--- src/pages/main/routes/user/searchResult.vue
+++ src/pages/main/routes/user/searchResult.vue
@@ -7,53 +7,54 @@
                     :data="hotel"
                     style="width: 100%"
                     max-height="510">
-<!--                <el-table-column
-                        prop="id"
-                        label="序号"
-                        width="150">
-                </el-table-column>-->
                 <el-table-column
                         prop="name"
                         label="名称"
                         sortable
-                        width="250">
+                        width="150" align="center" header-align="center" >
                 </el-table-column>
                 <el-table-column
                         prop="photo.photoUrl"
                         label="图片"
-                        width="200">
+                        width="300" align="center" header-align="center" >
+                    <template v-slot="scope">
+                        <img :src="scope.row.photo.photoUrl" alt="" width="90" height="90">
+                    </template>
                 </el-table-column>
-              <el-table-column label="地址">
+              <el-table-column label="地址" align="center" header-align="center" >
                 <el-table-column
                         prop="address.province"
                         label="省"
-                        width="90">
+                        width="120" align="center" header-align="center" >
                 </el-table-column><el-table-column
                         prop="address.city"
                         label="市"
-                        width="90">
-                </el-table-column><el-table-column
+                        width="120" align="center" header-align="center" >
+                </el-table-column>
+                  <el-table-column
                         prop="address.district"
                         label="区"
-                        width="90">
+                        width="120" align="center" header-align="center" >
                 </el-table-column>
               </el-table-column>
                 <el-table-column
                         prop="room.price"
                         label="最低价格"
                         sortable
-                        width="200">
+                        width="150" align="center" header-align="center" >
                 </el-table-column>
               <el-table-column
                   prop="star"
                   label="星级"
                   sortable
-                  width="200">
+                  width="200" align="center" header-align="center" >
+                  <template slot-scope="scope" >
+                      <el-rate v-model="scope.row.star" :allow-half="true"  disabled text-color="#ff9900"></el-rate>
+                  </template>
               </el-table-column>
                 <el-table-column
-                        fixed="right"
                         label="操作"
-                        width="160">
+                        align="center" header-align="center" >
                     <el-button
                             @click.native.prevent="deleteRow(scope.$index, hotelData)"
                             type="text"
