07915e981c0aa68543a1f87284aeff6849f59c5d
mr <18292166117@163.com>
2022-03-09 10:33:36 +0800

    折线图

--- src/pages/main/routes/admin/orderCharts.vue
+++ src/pages/main/routes/admin/orderCharts.vue
@@ -77,24 +77,13 @@ export default {
       // 绘制图表
       this.mStudyTime.setOption({
         title: {
-          text: 'Stacked Area Chart'
+          text: 'Stacked Line'
         },
         tooltip: {
-          trigger: 'axis',
-          axisPointer: {
-            type: 'cross',
-            label: {
-              backgroundColor: '#6a7985'
-            }
-          }
+          trigger: 'axis'
         },
         legend: {
           data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-        },
-        toolbox: {
-          feature: {
-            saveAsImage: {}
-          }
         },
         grid: {
           left: '3%',
@@ -102,71 +91,48 @@ export default {
           bottom: '3%',
           containLabel: true
         },
-        xAxis: [
-          {
-            type: 'category',
-            boundaryGap: false,
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        toolbox: {
+          feature: {
+            saveAsImage: {}
           }
-        ],
-        yAxis: [
-          {
-            type: 'value'
-          }
-        ],
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
         series: [
           {
             name: 'Email',
             type: 'line',
             stack: 'Total',
-            areaStyle: {},
-            emphasis: {
-              focus: 'series'
-            },
-            data: [120, 132, 101, 134, 90, 230, 510]
+            data: [120, 132, 101, 134, 90, 230, 210]
           },
           {
             name: 'Union Ads',
             type: 'line',
             stack: 'Total',
-            areaStyle: {},
-            emphasis: {
-              focus: 'series'
-            },
             data: [220, 182, 191, 234, 290, 330, 310]
           },
           {
             name: 'Video Ads',
             type: 'line',
             stack: 'Total',
-            areaStyle: {},
-            emphasis: {
-              focus: 'series'
-            },
             data: [150, 232, 201, 154, 190, 330, 410]
           },
           {
             name: 'Direct',
             type: 'line',
             stack: 'Total',
-            areaStyle: {},
-            emphasis: {
-              focus: 'series'
-            },
             data: [320, 332, 301, 334, 390, 330, 320]
           },
           {
             name: 'Search Engine',
             type: 'line',
             stack: 'Total',
-            label: {
-              show: true,
-              position: 'top'
-            },
-            areaStyle: {},
-            emphasis: {
-              focus: 'series'
-            },
             data: [820, 932, 901, 934, 1290, 1330, 1320]
           }
         ]
